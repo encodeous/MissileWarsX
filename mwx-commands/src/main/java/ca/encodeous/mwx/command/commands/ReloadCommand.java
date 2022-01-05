81cd19e9613a965e39381451102c8911a98c7ffb
Adam Chen <encodeous7@gmail.com>
2022-01-05 15:47:59 -0500

    Restrict reload to console only

--- mwx-commands/src/main/java/ca/encodeous/mwx/command/commands/ReloadCommand.java
+++ mwx-commands/src/main/java/ca/encodeous/mwx/command/commands/ReloadCommand.java
@@ -1,15 +1,16 @@
 package ca.encodeous.mwx.command.commands;
 
 import ca.encodeous.mwx.command.Command;
+import ca.encodeous.mwx.command.ExecutionSource;
 import ca.encodeous.mwx.command.MissileWarsCommand;
 import ca.encodeous.mwx.command.RootCommand;
 import ca.encodeous.mwx.core.game.CoreGame;
 
-public class mwreload extends MissileWarsCommand {
+public class ReloadCommand extends MissileWarsCommand {
 
     @Override
     public RootCommand BuildCommand() {
-        return new RootCommand("mwreload", Command::DefaultAdminCommand).Executes((context) -> {
+        return new RootCommand("mwreload", Command::DefaultAdminCommand).Executes(ExecutionSource.CONSOLE, (context) -> {
             CoreGame.Instance.Reload();
             context.SendMessage("&aSuccessfully reloaded game");
             return 1;
