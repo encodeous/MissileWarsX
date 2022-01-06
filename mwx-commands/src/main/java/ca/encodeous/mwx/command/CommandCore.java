dea5f9b6487d6c44a98e8b04aedadeefbf51148f
Adam Chen <encodeous7@gmail.com>
2022-01-06 07:57:38 -0500

    Add short `mw<lobby>` commands to registry

--- mwx-commands/src/main/java/ca/encodeous/mwx/command/CommandCore.java
+++ mwx-commands/src/main/java/ca/encodeous/mwx/command/CommandCore.java
@@ -1,6 +1,9 @@
 package ca.encodeous.mwx.command;
 
+import ca.encodeous.mwx.command.commands.LobbyCommand;
+import ca.encodeous.mwx.core.game.CoreGame;
 import ca.encodeous.mwx.core.utils.Reflection;
+import ca.encodeous.mwx.engines.lobby.LobbyEngine;
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import org.bukkit.Bukkit;
@@ -25,15 +28,19 @@ public class CommandCore {
         MissileWarsCommand cmd = (MissileWarsCommand) Reflection.newInstance(
                 Objects.requireNonNull(Reflection.getConstructor(missileWarsCommand)));
 
+        cmd.BuildCommand().Register(GetDispatcher());
 
+        return true;
+    }
 
+    public CommandDispatcher<Object> GetDispatcher(){
         Object dedicatedServer = Reflection.invokeMethod("getServer", Bukkit.getServer());
         Class<?> minecraftServer = dedicatedServer.getClass().getSuperclass();
-        Object vanillaCommandDispatcher = Reflection.get(minecraftServer, "vanillaCommandDispatcher", dedicatedServer);
-        if(vanillaCommandDispatcher != null) {
-            CommandDispatcher<Object> commandDispatcher = Reflection.get("g", vanillaCommandDispatcher);
+        var dispatch = Reflection.get(minecraftServer, "vanillaCommandDispatcher", dedicatedServer);
+        if(dispatch != null) {
+            CommandDispatcher<Object> commandDispatcher = Reflection.get("g", dispatch);
             if(commandDispatcher != null) {
-                cmd.BuildCommand().Register(commandDispatcher);
+                return commandDispatcher;
             }else {
                 throw new RuntimeException("Could not get access to the Brigadier Command Dispatcher");
             }
@@ -41,7 +48,6 @@ public class CommandCore {
             throw new RuntimeException("Vanilla Command Dispatcher not found");
         }
 
-        return true;
     }
 
     public void RegisterAllCommands(){
@@ -57,6 +63,23 @@ public class CommandCore {
             }
         }
         plugin.getLogger().info("Registered " + cnt + " MissileWars commands.");
+        RegisterLobbies();
+    }
+
+    public void RegisterLobbies(){
+        plugin.getLogger().info("Registering lobby commands...");
+        int i = 1;
+        for(var lobby : CoreGame.Instance.mwLobbies.Lobbies) {
+            int lid = i++;
+            var cmd = new RootCommand("mw" + lid, Command::DefaultPlayerCommand)
+                    .Executes(ExecutionSource.PLAYER, context->{
+                        LobbyEngine.FromPlayer(context.GetSendingPlayer()).lobby.RemovePlayer(context.GetSendingPlayer());
+                        LobbyEngine.GetLobby(lid).AddPlayer(context.GetSendingPlayer());
+                        context.SendMessage("&9You have been teleported to lobby " + lobby + ".");
+                        return 1;
+                    });
+            cmd.Register(GetDispatcher());
+        }
     }
 
     public Set<Class<? extends MissileWarsCommand>> getClasses() {
